ee35cb13d1fa1f733bd31ed3f010e4d180197a95
Michael Lobman <michael.lobman@gmail.com>
2022-01-11 21:16:22 -0500

    Complete assignment

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+    if array.length == 1
+        array.join
+    elsif array.length == 2
+        "#{array[0]} and #{array[1]}"
+    else
+        final = array.pop
+        first = array.join(", ")
+        "#{first}, and #{final}"
+    end
 end
