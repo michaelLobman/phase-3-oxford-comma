6383f73ee4063204244bd4651bd609ee543c0ed0
Michael Lobman <michael.lobman@gmail.com>
2022-01-11 21:15:52 -0500

    commit tree d61a732b61130a3709bbd599ded00d6c317449e3

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,20 @@
-def oxford_comma(array)
+# take in array of words
+# return string with an oxford comma
+# ["dylan", "buckley", "cash"] 
+# => "dylan, buckley, and cash"
 
+test_array = ["dylan", "buckley", "cash"]
+
+def oxford_comma(array)
+    if array.length == 1
+        array.join
+    elsif array.length == 2
+        "#{array[0]} and #{array[1]}"
+    else
+        final = array.pop
+        first = array.join(", ")
+        "#{first}, and #{final}"
+    end
 end
+
+puts oxford_comma test_array
